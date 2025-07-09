b3d07920577471157b6a591fc0dfcf57ef7ee6b0
gugavalenca <gustavo_valenca@hotmail.com>
2025-07-09 12:57:00 -0300

    Add initial implementation of Todo list with styling and components

--- to_do_react/src/App.jsx
+++ to_do_react/src/App.jsx
@@ -1,7 +1,51 @@
 import { useState } from "react";
+import Todo from "./components/Todo";
+import "./App.css"; // Importando o CSS para estilização
 
 function App() {
-  return <div>Olá React!</div>;
+
+  const[todos, setTodos] = useState([
+
+    { 
+      id: 1, 
+      text: "Estudar React",
+      category: "Estudos",
+      iscompleted: false,
+     },
+    { 
+      id: 2, 
+      text: "Ir para a academia ",
+      category: "Pessoal",
+      iscompleted: false,
+     },
+    { 
+      id: 3, 
+      text: "Meeting com a equipe",
+      category: "Trabalho",
+      iscompleted: false,
+     },
+
+  ]);
+
+  return (
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <div className="todo">
+            <div className="content">
+              <p>{todo.text}</p>
+              <p className="category">({todo.category})</p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>x</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
