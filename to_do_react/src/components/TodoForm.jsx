e2aa6fa0009c9540e46f03f823f6345f29fab070
gugavalenca <gustavo_valenca@hotmail.com>
2025-07-09 13:28:04 -0300

    Refactor TodoForm component and update styling in App.css

--- to_do_react/src/components/TodoForm.jsx
+++ to_do_react/src/components/TodoForm.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const TodoForm = () => {
-  return (
-    <div>TodoForm</div>
-  )
-}
+  return <div className="todo-form">
+    <h2>Criar Tarefa</h2>
+    <form>
+        <input type="text" placeholder="Digite o Título" />
+        <select>
+            <option value="">Selecione uma categoria</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Pessoal">Pessoal</option>
+            <option value="Estudos">Estudos</option>
+        </select>
+        <button type="submit" className="todo-button">Adicionar Tarefa</button>
+    </form>
+  </div>
+};
 
-export default TodoForm
+export default TodoForm;
